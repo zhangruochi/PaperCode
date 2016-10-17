21bc1d73f8a62ca04a3a6b1e7f390f1e878fec1d
lvduzhen@gmail.com <lvduzhen@gmail.com>
2016-10-17 23:15:18 +0800

    GUI

--- paper3/BestKFirstGUI.py
+++ paper3/BestKFirstGUI.py
@@ -1,94 +1,157 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import messagebox
+import tkinter.filedialog
+from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askopenfilename
+from algorithm import single_stop
 
 class App(ttk.Frame):
     def __init__(self,master=None):
         super().__init__(master)
+
+        self.dataset_filename = None
+        self.label_filename = None
+
         self.master = master
         self.master.title("异构数据融合")
-        self.master.geometry("1000x600+300+200")
+        self.master.geometry("1000x650+50+20")
         self.pack(fill=tk.BOTH,expand=1)
-        self.operation_panel = ttk.Frame(self)
-        self.operation_panel.grid(column=0)
-        self.output_panel = ttk.Frame(self)
-        self.output_panel.grid(column=1)
+        
+        #添加菜单
+        menubar = tk.Menu(self.master)
+        self.master.config(menu = menubar)
+
+        menu_button_main = tk.Menu(menubar)
+        menu_button_main.add_command(label="Exit",command=self.__exit_app)
+        menu_button_help = tk.Menu(menubar)
+        menu_button_help.add_command(label="Website")
+        menubar.add_cascade(label="Main",menu=menu_button_main)
+        menubar.add_cascade(label="Help",menu=menu_button_help)
+
+
+
+        #分块
+        self.left_panel = ttk.Frame(self)
+        self.left_panel.grid(row=0,column=0)
+        self.right_panel = ttk.Frame(self)
+        self.right_panel.grid(row=0,column=1,columnspan=3)
 
         self.initial_app()
 
 
     def initial_app(self): 
-        
 
+#---------布局: 分为五个大板块（左三右二） ----------        
         style = ttk.Style()
-        style.configure("OR.TLabel", foreground="black", background="wite")
+        style.configure("UP.TButton", height=5,width=15)
+        
+        choose_panel = ttk.Frame(self.left_panel,borderwidt=30)
+        choose_panel.grid(row=0)
+        upload_panel = ttk.Frame(self.left_panel,borderwidt=30)
+        upload_panel.grid(row=1)
+        control_panel = ttk.Frame(self.left_panel,borderwidt=30)
+        control_panel.grid(row=2)
+
+  
+        visual_panel = ttk.Frame(self.right_panel,borderwidt=30)
+        visual_panel.grid(row=0,rowspan=3)
         
         
-        #选择标签
+#--------------- 左1------------------
+        ttk.Label(choose_panel,text="select your mode").grid(row=0,columnspan=2,sticky=tk.W)
 
         var_gene = tk.IntVar()
         var_image = tk.IntVar()
-        var_eec = tk.IntVar()    
-        ttk.Checkbutton(self.operation_panel,text="gene",variable = var_gene).grid(row=0,column=0,sticky=tk.W)
-        ttk.Checkbutton(self.operation_panel,text="image",variable = var_image).grid(row=1,column=0,sticky=tk.W)
-        ttk.Checkbutton(self.operation_panel,text="eec",variable = var_eec).grid(row=3,column=0,sticky=tk.W)
-
-        ttk.Button(self.operation_panel,text="confirm",command= lambda: self.generate_button(var_gene,var_image,var_eec)).grid(row=4,column=0,sticky=tk.W)
-        ttk.Button(self.operation_panel,text="clear").grid(row=4,column=1,sticky=tk.W)
-
-
-    
-    def generate_button(self,var_gene,var_image,var_eec):
-        if var_gene.get() == 1 and var_image.get() == 0 and var_eec.get() == 0:
-            ttk.Button(self.operation_panel,text="upload gene dataset").grid(row=6,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload gene label").grid(row=6,column=1,sticky=tk.W)
-        elif var_gene.get() == 0 and var_image.get() == 1 and var_eec.get() == 0:
-            ttk.Button(self.operation_panel,text="upload image dataset").grid(row=7,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload image label").grid(row=7,column=1,sticky=tk.W)
-        elif var_gene.get() == 0 and var_image.get() == 0 and var_eec.get() == 1:
-            ttk.Button(self.operation_panel,text="upload EEC dataset").grid(row=8,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload EEC label").grid(row=8,column=1,sticky=tk.W)
-
-        elif var_gene.get() == 1 and var_image.get() == 1 and var_eec.get() == 0:
-            ttk.Button(self.operation_panel,text="upload gene dataset").grid(row=6,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload gene label").grid(row=6,column=1,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload image dataset").grid(row=7,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload image label").grid(row=7,column=1,sticky=tk.W) 
-        elif var_gene.get() == 1 and var_image.get() == 0 and var_eec.get() == 1:
-            ttk.Button(self.operation_panel,text="upload gene dataset").grid(row=6,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload gene label").grid(row=6,column=1,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload EEC dataset").grid(row=7,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload EEC label").grid(row=7,column=1,sticky=tk.W)
-
-        elif var_gene.get() == 0 and var_image.get() == 1 and var_eec.get() == 1:
-            ttk.Button(self.operation_panel,text="upload image dataset").grid(row=6,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload image label").grid(row=6,column=1,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload EEC dataset").grid(row=7,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload EEC label").grid(row=7,column=1,sticky=tk.W)
-        elif var_gene.get() == 1 and var_image.get() == 1 and var_eec.get() == 1:  
-            ttk.Button(self.operation_panel,text="upload gene dataset").grid(row=6,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload gene label").grid(row=6,column=1,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload image dataset").grid(row=7,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload image label").grid(row=7,column=1,sticky=tk.W)  
-            ttk.Button(self.operation_panel,text="upload EEC dataset").grid(row=8,column=0,sticky=tk.W)
-            ttk.Button(self.operation_panel,text="upload EEC label").grid(row=8,column=1,sticky=tk.W)
-        else: 
-            messagebox.showwarning("error","you have not choose a kind of dataset!") 
-
-    """
-    def getGene(self,var):
-        return var.get()
-
-    def getImage(self,var):
-        return var.get()
-
-    def getEec(self,var):
-        return var.get()  
-    """          
-
+        var_eec = tk.IntVar()
+        
+        gene_button = ttk.Checkbutton(choose_panel,text="gene",variable = var_gene)
+        image_button = ttk.Checkbutton(choose_panel,text="image",variable = var_image)
+        eec_button = ttk.Checkbutton(choose_panel,text="eec",variable = var_eec)
+        
+        gene_button.grid(row=1,columnspan=2,sticky=tk.W)
+        image_button.grid(row=2,columnspan=2,sticky=tk.W)
+        eec_button.grid(row=3,columnspan=2,sticky=tk.W)
         
 
+#--------------- 左2------------------
 
+        ttk.Button(upload_panel,text="Upload gene dataset",style="UP.TButton",command = lambda: self.load_gene_dataset(var_gene)).grid(row=0,column=0,sticky=tk.W)
+        ttk.Button(upload_panel,text="Upload gene label",style="UP.TButton",command = lambda: self.load_gene_label(var_gene)).grid(row=0,column=1,sticky=tk.W)
+        ttk.Button(upload_panel,text="Upload image dataset",style="UP.TButton",command = lambda: self.load_image_dataset(var_image)).grid(row=1,column=0,sticky=tk.W)
+        ttk.Button(upload_panel,text="Upload image label",style= "UP.TButton",command = lambda: self.load_image_label(var_image)).grid(row=1,column=1,sticky=tk.W)  
+        ttk.Button(upload_panel,text="Upload EEC dataset",style="UP.TButton",command = lambda: self.load_eec_dataset(var_eec)).grid(row=2,column=0,sticky=tk.W)
+        ttk.Button(upload_panel,text="Upload EEC label",style="UP.TButton",command = lambda: self.load_eec_label(var_eec)).grid(row=2,column=1,sticky=tk.W)
+
+#--------------- 左3------------------
+
+        ttk.Button(control_panel,text="Start",command = self.start_algorithm).grid(row=0,column=0,sticky=tk.W)
+        ttk.Button(control_panel,text="End").grid(row=0,column=1,sticky=tk.W)
+
+
+#--------------- 右1---------------
+        ttk.Label(visual_panel,text="Best \"k\" First ").grid(row=0,column=0,sticky=tk.W)
+        variable = tk.StringVar(visual_panel)
+        option_menu = ttk.OptionMenu(visual_panel, variable, "1","2","3","4","5","6","7")
+        option_menu.grid(row=0,column=1)
+        completed_label = ttk.Label(visual_panel,text="进度条")
+        completed_label.grid(row=2,columnspan=2,sticky=tk.W)
+
+
+#--------------- 右2---------------
+        output_panel = tk.Canvas(self.right_panel, width=500, height=500)
+        output_panel.grid(rowspan=7)
+    
+
+    def __exit_app(self):
+        exit()
+
+
+    def load_gene_dataset(self,var):
+        if var.get() == 1:
+            file_name = askopenfilename(filetypes=(("TXT Files", "*.txt"),("CSV Files", "*.csv")))
+            self.dataset_filename = file_name
+        else:
+            messagebox.showerror("ERROR!","You have not choose the gene")
+
+    def load_gene_label(self,var):
+        if var.get() == 1:
+            file_name = askopenfilename(filetypes=(("TXT Files", "*.txt"),("CSV Files", "*.csv")))
+            self.label_filename = file_name
+        else:
+            messagebox.showerror("ERROR!","You have not choose the gene")        
+
+    def load_image_dataset(self,var):
+        if var.get() == 1:
+            print("load_image_dataset")
+        else:
+            messagebox.showerror("ERROR!","You have not choose the image")
+
+    def load_image_label(self,var):
+        if var.get() == 1:
+            print("load_image_label")
+        else:
+            messagebox.showerror("ERROR!","You have not choose the image")        
+
+    def load_eec_dataset(self,var):
+        if var.get() == 1:
+            print("load_eec_dataset")
+        else:
+            messagebox.showerror("ERROR!","You have not choose the eec")  
+
+    def load_eec_label(self,var):
+        if var.get() == 1:
+            print("load_eec_label")
+        else:
+            messagebox.showerror("ERROR!","You have not choose the eec")          
+
+                    
+    def start_algorithm(self):
+        if self.dataset_filename is not None and self.label_filename is not None:
+            print("starting the calculate {}".format(self.dataset_filename.split(".")[0]))
+            single_stop(self.dataset_filename,self.label_filename,1)
+
+        
         
 
 
---
+++ paper3/algorithm.py
@@ -0,0 +1,408 @@
+'''
+python3
+
+Required packages
+- pandas
+- numpy
+- sklearn
+
+Info
+- name   : "zhangruochi"
+- email  : "zrc720@gmail.com"
+- date   : "2016.10.12"
+- Version : 2.0.0
+
+Description
+    RIFS
+'''
+
+
+import numpy as np
+import pandas as pd
+import os
+import pickle
+import random
+import multiprocessing
+from functools import partial
+import time
+
+from scipy.stats import ttest_ind_from_stats
+
+from sklearn.cross_validation import KFold
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.grid_search import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+
+
+
+#加载数据集
+def load_data(filename):
+    full_path_name = os.path.join("/Users/ZRC/Desktop/HLab/dataset/data",filename)
+    dataset = pd.read_csv(full_path_name,index_col=0)
+    name_index_dic = get_name_index(dataset)
+    with open("name_index.pkl","wb") as f:
+        pickle.dump(name_index_dic,f)
+
+    dataset.columns = list(range(dataset.shape[1]))
+    dataset = dataset.rename(index = name_index_dic)
+    return dataset
+
+
+#创造特征索引和特征名字对应的字典 
+def get_name_index(dataset):
+    name_index_dic = {}
+    index = 0
+    for name in dataset.index:
+        name_index_dic[name] = index
+        index += 1 
+    return name_index_dic 
+
+
+#加载标签
+def load_class(filename):
+    full_path_name = os.path.join("/Users/ZRC/Desktop/HLab/dataset/class",filename)
+    class_set = pd.read_csv(full_path_name,index_col = 0)
+    labels = class_set["Class"]
+    result = []
+    
+    def convert(label):
+        if label == 'N':
+            result.append(0)
+        if label == 'P':
+            result.append(1)    
+
+    labels.apply(func = convert)     
+    return np.array(result)
+
+
+
+# t_检验  得到每个特征的 t 值
+def t_test(dataset,labels):
+    p_feature_data = dataset.loc[:,labels == 1]  #得到正类数据集
+    n_feature_data = dataset.loc[:,labels == 0]  #得到负类数据集
+
+    p_mean,n_mean = np.mean(p_feature_data,1),np.mean(n_feature_data,1)
+    p_std,n_std = np.std(p_feature_data,1),np.std(n_feature_data,1)
+
+    t_value,p_value = ttest_ind_from_stats(p_mean,p_std,p_feature_data.shape[1],n_mean,n_std,n_feature_data.shape[1])
+    p_value = pd.Series(data=p_value,index=list(range(len(p_value))))
+
+    return p_feature_data, n_feature_data, p_value
+ 
+
+
+#根据 t 检验的结果的大小重新构造特征集
+def rank_t_value(dataset,labels):
+    p_feature_data,n_feature_data,p_value = t_test(dataset,labels)
+    sort_index = p_value.sort_values(ascending=True).index
+
+    p_feature_data = p_feature_data.reindex(sort_index)
+    n_feature_data = n_feature_data.reindex(sort_index)
+
+    return p_feature_data.T,n_feature_data.T
+
+
+def prepare(datset_filename,class_filename):
+    dataset = load_data(datset_filename)
+    labels = load_class(class_filename)
+    p_feature_data,n_feature_data = rank_t_value(dataset,labels)
+    #将样本的顺序打乱  防止在交叉验证的时候出错
+    return p_feature_data,n_feature_data,labels
+
+
+
+#选择分类器 D-tree,SVM,NBayes,KNN 
+def select_estimator(case):
+
+    if case == 0:
+        estimator = SVC()
+    elif case == 1:
+        estimator = KNeighborsClassifier()
+    elif case == 2:
+        estimator = DecisionTreeClassifier()
+    elif case == 3:
+        estimator = GaussianNB()
+    elif case == 4:
+        estimator = LogisticRegression()    
+
+    return estimator            
+
+#采用 K-Fold 交叉验证 得到 aac 
+def get_aac(estimator,p_feature_data,n_feature_data,y,seed_number):
+    scores = []
+    k = 3
+    p_kf = KFold(p_feature_data.shape[0],n_folds = k,shuffle = True,random_state = seed_number)
+    n_kf = KFold(n_feature_data.shape[0],n_folds = k,shuffle = True,random_state = seed_number)
+
+    for i in range(k):
+
+        p_train_data = p_feature_data.iloc[list(p_kf)[i][0],:]
+        p_test_data = p_feature_data.iloc[list(p_kf)[i][1],:]
+
+        n_train_data = n_feature_data.iloc[list(n_kf)[i][0],:]
+        n_test_data = n_feature_data.iloc[list(n_kf)[i][1],:]
+
+        X_train = p_train_data.append(n_train_data)
+        y_train = y[X_train.index]
+        estimator.fit(X_train,y_train)
+
+                
+        X_test = p_test_data.append(n_test_data)
+        y_test = y[X_test.index]
+        scores.append(estimator.score(X_test,y_test))
+
+    return np.mean(scores)    
+
+# K-Fold  生成器
+def k_fold(y,k):
+
+    kf = KFold(len(y),n_folds = k)
+    for train_index,test_index in kf:
+        yield train_index,test_index
+        
+
+#生成重启的位置
+def random_num_generator(num_of_feature,seed_number,percent):
+    random.seed(seed_number)
+    return random.sample(list(range(num_of_feature)),int(num_of_feature*percent))   # 重启的组数为所有特征的一半
+
+
+"""
+def single_multiprocessing(args,percent):
+    seed_number = 7
+    start = time.time()
+    dataset_filename = args[0]
+    label_filename = args[1]
+    lock = args[2]
+
+    p_feature_data,n_feature_data,labels = prepare(dataset_filename,label_filename)
+    loc_of_first_feature = random_num_generator(p_feature_data.shape[1],seed_number,percent) # 重启的位置
+
+    max_loc_aac = 0
+    max_aac_list = []
+    estimator_list = [0,1,2,3,4]
+    feature_range = p_feature_data.shape[1]
+
+    if not os.path.exists("{}".format(percent)):
+        os.mkdir("{}".format(percent))
+    
+    for loc in loc_of_first_feature:
+        num = 0
+        max_k_aac = 0 
+        count = 0  #记录相等的次数
+        best_estimator = -1   
+        
+        for k in range(feature_range - loc):  # 从 loc位置 开始选取k个特征
+            max_estimator_aac = 0
+            locs = [i for i in range(loc,loc+k+1)]
+
+            p_data = p_feature_data.iloc[:,locs]
+            n_data = n_feature_data.iloc[:,locs]
+
+            for item in estimator_list:
+                estimator_aac = get_aac(select_estimator(item),p_data,n_data,labels,seed_number)
+                if estimator_aac > max_estimator_aac:
+                    max_estimator_aac = estimator_aac   #记录对于 k 个 特征 用四个estimator 得到的最大值
+                    best_temp_estimator = item
+     
+            if max_estimator_aac > max_k_aac:
+                count = 0 
+                max_k_aac = max_estimator_aac   #得到的是从 loc 开始重启的最大值
+                num = k+1
+                best_estimator = best_temp_estimator
+            
+            else:
+                count += 1
+                if count == 3:
+                    break
+   
+        if max_k_aac > max_loc_aac:
+            max_loc_aac = max_k_aac
+            max_aac_list = []
+            max_aac_list.append((loc,num,max_loc_aac,best_estimator))
+            print(">: {}\n".format(max_aac_list))
+            with lock:
+                infor_file = open("{}/{}_outpot.txt".format(percent,dataset_filename.split(".")[0]),"a")
+                infor_file.write(">: {}\n".format(max_aac_list))
+                infor_file.close()
+            
+
+        elif max_k_aac == max_loc_aac:
+            max_aac_list.append((loc,num,max_loc_aac,best_estimator))
+            print("=: {}\n".format(max_aac_list))
+            with lock:
+                infor_file = open("{}/{}_outpot.txt".format(percent,dataset_filename.split(".")[0]),"a")
+                infor_file.write("=: {}\n".format(max_aac_list))
+                infor_file.close()
+
+    end = time.time()
+    with lock:
+        infor_file = open("{}/{}_outpot.txt".format(percent,dataset_filename.split(".")[0]),"a")
+        infor_file.write("using time: {}".format(end-start))  
+        infor_file.close()          
+
+    return max_aac_list 
+
+
+
+def test_dataset_multiprocessing():
+    dataset_filename = "Gastric1.csv"
+    label_filename = "Gastric1class.csv"
+    percents = [ i/10 for i in range(1,10)]
+    single_dataset= partial(single,[dataset_filename,label_filename,lock])
+
+    pool = multiprocessing.Pool(3)
+    lock = multiprocessing.Manager().Lock()
+    result = pool.map(single_dataset,percents)
+
+    pool.close()
+    pool.join()
+    
+    with open("output.txt","a") as f:
+        f.write(str(results))
+
+"""
+
+def single(dataset_filename,label_filename,percent):
+    seed_number = 7
+    start = time.time()
+
+    p_feature_data,n_feature_data,labels = prepare(dataset_filename,label_filename)
+    loc_of_first_feature = random_num_generator(p_feature_data.shape[1],seed_number,percent) # 重启的位置
+
+    max_loc_aac = 0
+    max_aac_list = []
+    estimator_list = [0,1,2,3,4]
+    feature_range = p_feature_data.shape[1]
+
+    if not os.path.exists("{}".format(percent)):
+        os.mkdir("{}".format(percent))
+    
+    for loc in loc_of_first_feature:
+        num = 0
+        max_k_aac = 0 
+        count = 0  #记录相等的次数
+        best_estimator = -1   
+        
+        for k in range(feature_range - loc):  # 从 loc位置 开始选取k个特征
+            max_estimator_aac = 0
+            locs = [i for i in range(loc,loc+k+1)]
+
+            p_data = p_feature_data.iloc[:,locs]
+            n_data = n_feature_data.iloc[:,locs]
+
+            for item in estimator_list:
+                estimator_aac = get_aac(select_estimator(item),p_data,n_data,labels,seed_number)
+                if estimator_aac > max_estimator_aac:
+                    max_estimator_aac = estimator_aac   #记录对于 k 个 特征 用四个estimator 得到的最大值
+                    best_temp_estimator = item
+     
+            if max_estimator_aac > max_k_aac:
+                count = 0 
+                max_k_aac = max_estimator_aac   #得到的是从 loc 开始重启的最大值
+                num = k+1
+                best_estimator = best_temp_estimator
+            
+            else:
+                count += 1
+                if count == 3:
+                    break
+   
+        if max_k_aac > max_loc_aac:
+            max_loc_aac = max_k_aac
+            max_aac_list = []
+            max_aac_list.append((loc,num,max_loc_aac,best_estimator))
+            print(">: {}\n".format(max_aac_list))
+            with open("{}/{}_outpot.txt".format(percent,dataset_filename.split(".")[0]),"a") as infor_file:
+                infor_file.write(">: {}\n".format(max_aac_list))
+
+        elif max_k_aac == max_loc_aac:
+            max_aac_list.append((loc,num,max_loc_aac,best_estimator))
+            print("=: {}\n".format(max_aac_list))
+            with open("{}/{}_outpot.txt".format(percent,dataset_filename.split(".")[0]),"a") as infor_file:
+                infor_file.write("=: {}\n".format(max_aac_list))
+
+    end = time.time()
+    with open("{}/{}_outpot.txt".format(percent,dataset_filename.split(".")[0]),"a") as infor_file:
+        infor_file.write("using time: {}".format(end-start))            
+
+    return max_aac_list 
+
+def single_stop(dataset_filename,label_filename,stop):
+    seed_number = 7
+    start = time.time()
+
+    p_feature_data,n_feature_data,labels = prepare(dataset_filename,label_filename)
+    loc_of_first_feature = random_num_generator(p_feature_data.shape[1],seed_number,0.4) # 重启的位置
+
+    max_loc_aac = 0
+    max_aac_list = []
+    estimator_list = [0,1,2,3,4]
+    feature_range = p_feature_data.shape[1]
+
+    if not os.path.exists("{}".format(stop)):
+        os.mkdir("{}".format(stop))
+    
+    for loc in loc_of_first_feature:
+        num = 0
+        max_k_aac = 0 
+        count = 0  #记录相等的次数
+        best_estimator = -1   
+        
+        for k in range(feature_range - loc):  # 从 loc位置 开始选取k个特征
+            max_estimator_aac = 0
+            locs = [i for i in range(loc,loc+k+1)]
+
+            p_data = p_feature_data.iloc[:,locs]
+            n_data = n_feature_data.iloc[:,locs]
+
+            for item in estimator_list:
+                estimator_aac = get_aac(select_estimator(item),p_data,n_data,labels,seed_number)
+                if estimator_aac > max_estimator_aac:
+                    max_estimator_aac = estimator_aac   #记录对于 k 个 特征 用四个estimator 得到的最大值
+                    best_temp_estimator = item
+            
+            if max_estimator_aac > max_k_aac:
+                count = 0 
+                max_k_aac = max_estimator_aac   #得到的是从 loc 开始重启的最大值
+                num = k+1
+                best_estimator = best_temp_estimator
+            
+            else:
+                count += 1
+                if count == stop:
+                    break
+   
+        if max_k_aac > max_loc_aac:
+            max_loc_aac = max_k_aac
+            max_aac_list = []
+            max_aac_list.append((loc,num,max_loc_aac,best_estimator))
+            print(">: {}\n".format(max_aac_list))
+            
+        elif max_k_aac == max_loc_aac:
+            max_aac_list.append((loc,num,max_loc_aac,best_estimator))
+            print("=: {}\n".format(max_aac_list))
+            
+    end = time.time()
+    return max_aac_list             
+
+def test_dataset():
+    dataset_filename = "Gastric2.csv"
+    label_filename = "Gastric2class.csv"
+    stop=1
+    single_stop(dataset_filename,label_filename,stop)
+      
+
+
+if __name__ == '__main__':
+    test_dataset()
+      
+
+    
+    
+    
+
+     
