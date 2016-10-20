889a5de56cea6eefd6caca3054f6657132dbdc88
lvduzhen@gmail.com <lvduzhen@gmail.com>
2016-10-20 09:24:08 +0800

    2016.10.19

--- paper3/BestKFirstGUI.py
+++ paper3/BestKFirstGUI.py
@@ -6,6 +6,9 @@ from tkinter.filedialog import askdirectory
 from tkinter.filedialog import askopenfilename
 from algorithm import single_stop
 
+#grid (-column, -columnspan, -in, -ipadx, -ipady, -padx, -pady, -row, -rowspan, or -sticky)
+#pack (-after, -anchor, -before, -expand, -fill, -in, -ipadx, -ipady, -padx, -pady, or -side)
+
 class App(ttk.Frame):
     def __init__(self,master=None):
         super().__init__(master)
@@ -42,122 +45,81 @@ class App(ttk.Frame):
 
     def initial_app(self): 
 
-#---------布局: 分为五个大板块（左三右二） ----------        
+#---------布局: 分为4个大板块（左三右一） ----------        
         style = ttk.Style()
-        style.configure("UP.TButton", height=5,width=15)
+        style.configure("UP.TButton",height=5, width=13)
+        style.configure("OU.TLabel",font=('Helvetica', 20))
+        style.configure("SE.TLabel",font=('Helvetica', 15))
+        style.configure("SE.TEntry",width=10)
         
-        choose_panel = ttk.Frame(self.left_panel,borderwidt=30)
-        choose_panel.grid(row=0)
+        self.choose_panel = ttk.Frame(self.left_panel,borderwidt=30)
+        self.choose_panel.pack(fill=tk.BOTH,expand=1,side=tk.TOP)
         upload_panel = ttk.Frame(self.left_panel,borderwidt=30)
-        upload_panel.grid(row=1)
+        upload_panel.pack(fill=tk.BOTH,expand=1)
         control_panel = ttk.Frame(self.left_panel,borderwidt=30)
-        control_panel.grid(row=2)
-
-  
-        visual_panel = ttk.Frame(self.right_panel,borderwidt=30)
-        visual_panel.grid(row=0,rowspan=3)
+        control_panel.pack(fill=tk.BOTH,expand=1,side=tk.BOTTOM)
         
         
 #--------------- 左1------------------
-        ttk.Label(choose_panel,text="select your mode").grid(row=0,columnspan=2,sticky=tk.W)
+        ttk.Label(self.choose_panel,text="Select your algorithm",style="SE.TLabel").grid(row=0,column=0,padx=10,sticky=tk.W)
 
-        var_gene = tk.IntVar()
-        var_image = tk.IntVar()
-        var_eec = tk.IntVar()
-        
-        gene_button = ttk.Checkbutton(choose_panel,text="gene",variable = var_gene)
-        image_button = ttk.Checkbutton(choose_panel,text="image",variable = var_image)
-        eec_button = ttk.Checkbutton(choose_panel,text="eec",variable = var_eec)
-        
-        gene_button.grid(row=1,columnspan=2,sticky=tk.W)
-        image_button.grid(row=2,columnspan=2,sticky=tk.W)
-        eec_button.grid(row=3,columnspan=2,sticky=tk.W)
+        var = tk.StringVar(self.choose_panel)
+        option_menu = ttk.OptionMenu(self.choose_panel, var, "RIFS","RIFS","BEST K FIRST","COMPLETED",command = lambda x: self.__generate_para(var))
+        option_menu.grid(row=0,column = 1,columnspan = 3,sticky=tk.W)
         
 
 #--------------- 左2------------------
-
-        ttk.Button(upload_panel,text="Upload gene dataset",style="UP.TButton",command = lambda: self.load_gene_dataset(var_gene)).grid(row=0,column=0,sticky=tk.W)
-        ttk.Button(upload_panel,text="Upload gene label",style="UP.TButton",command = lambda: self.load_gene_label(var_gene)).grid(row=0,column=1,sticky=tk.W)
-        ttk.Button(upload_panel,text="Upload image dataset",style="UP.TButton",command = lambda: self.load_image_dataset(var_image)).grid(row=1,column=0,sticky=tk.W)
-        ttk.Button(upload_panel,text="Upload image label",style= "UP.TButton",command = lambda: self.load_image_label(var_image)).grid(row=1,column=1,sticky=tk.W)  
-        ttk.Button(upload_panel,text="Upload EEC dataset",style="UP.TButton",command = lambda: self.load_eec_dataset(var_eec)).grid(row=2,column=0,sticky=tk.W)
-        ttk.Button(upload_panel,text="Upload EEC label",style="UP.TButton",command = lambda: self.load_eec_label(var_eec)).grid(row=2,column=1,sticky=tk.W)
-
+        dataset_panel = tk.Canvas(upload_panel)
+        dataset_panel.grid(columnspan = 2)
+        ttk.Button(upload_panel,text="Upload dataset file",style="UP.TButton",command = self.load_dataset).grid(row=3,column=0,sticky=tk.W)
+        ttk.Button(upload_panel,text="Upload label file",style="UP.TButton",command = self.load_label).grid(row=3,column=1,sticky=tk.W)
+        
 #--------------- 左3------------------
 
-        ttk.Button(control_panel,text="Start",command = self.start_algorithm).grid(row=0,column=0,sticky=tk.W)
-        ttk.Button(control_panel,text="End").grid(row=0,column=1,sticky=tk.W)
+        ttk.Button(control_panel,text="Start",command = self.start_algorithm).grid(row=0,column=0,pady=30,padx=35,sticky=tk.E)
+        ttk.Button(control_panel,text="Stop",command = self.__exit_app).grid(row=0,column=1,pady=30,sticky=tk.W)
 
 
-#--------------- 右1---------------
-        ttk.Label(visual_panel,text="Best \"k\" First ").grid(row=0,column=0,sticky=tk.W)
-        variable = tk.StringVar(visual_panel)
-        option_menu = ttk.OptionMenu(visual_panel, variable, "1","2","3","4","5","6","7")
-        option_menu.grid(row=0,column=1)
-        completed_label = ttk.Label(visual_panel,text="进度条")
-        completed_label.grid(row=2,columnspan=2,sticky=tk.W)
-
-
-#--------------- 右2---------------
-        output_panel = tk.Canvas(self.right_panel, width=500, height=500)
-        output_panel.grid(rowspan=7)
+#--------------- 右---------------
+        ttk.Label(self.right_panel,text="OUTPUT",style="OU.TLabel").grid(row=0,rowspan=3,pady=20,padx=20)    
+        output_panel = tk.Canvas(self.right_panel, width=550, height=550)
+        output_panel.grid(row=3,padx=20)
     
 
     def __exit_app(self):
         exit()
 
+    def __generate_para(self,variable):
+        if variable.get() == "RIFS":
+            ttk.Label(self.choose_panel,text="Input percent").grid(row=1,column=0,padx=10,sticky=tk.W)
+            ttk.Entry(self.choose_panel,style="SE.TEntry").grid(row=1,column=1)
+            ttk.Label(self.choose_panel,text="Input Stop").grid(row=2,column=0,padx=10,sticky=tk.W)
+            ttk.Entry(self.choose_panel,style="SE.TEntry").grid(row=2,column=1)
 
-    def load_gene_dataset(self,var):
-        if var.get() == 1:
+
+
+    def load_dataset(self):
+        try:
             file_name = askopenfilename(filetypes=(("TXT Files", "*.txt"),("CSV Files", "*.csv")))
             self.dataset_filename = file_name
-        else:
-            messagebox.showerror("ERROR!","You have not choose the gene")
+        except:
+            messagebox.showerror("ERROR!","Load file unsuccessful!")
 
-    def load_gene_label(self,var):
-        if var.get() == 1:
+    def load_label(self):
+        try:
             file_name = askopenfilename(filetypes=(("TXT Files", "*.txt"),("CSV Files", "*.csv")))
-            self.label_filename = file_name
-        else:
-            messagebox.showerror("ERROR!","You have not choose the gene")        
+            self.dataset_filename = file_name
+        except:
+            messagebox.showerror("ERROR!","Load file unsuccessful!")        
 
-    def load_image_dataset(self,var):
-        if var.get() == 1:
-            print("load_image_dataset")
-        else:
-            messagebox.showerror("ERROR!","You have not choose the image")
-
-    def load_image_label(self,var):
-        if var.get() == 1:
-            print("load_image_label")
-        else:
-            messagebox.showerror("ERROR!","You have not choose the image")        
-
-    def load_eec_dataset(self,var):
-        if var.get() == 1:
-            print("load_eec_dataset")
-        else:
-            messagebox.showerror("ERROR!","You have not choose the eec")  
-
-    def load_eec_label(self,var):
-        if var.get() == 1:
-            print("load_eec_label")
-        else:
-            messagebox.showerror("ERROR!","You have not choose the eec")          
-
-                    
+         
     def start_algorithm(self):
         if self.dataset_filename is not None and self.label_filename is not None:
             print("starting the calculate {}".format(self.dataset_filename.split(".")[0]))
             single_stop(self.dataset_filename,self.label_filename,1)
-
-        
-        
-
-
-        
-
-
+        else:
+            messagebox.showerror("ERROR!","You have not load the dataset!")
+                
 
 
 if __name__ == '__main__':
