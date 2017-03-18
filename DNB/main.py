eb29ee20327cc23b840499ad2e110254cde6edea
zhangruochi <zrc720@gmail.com>
2017-03-18 22:22:46 +0800

    2017.03.18

--- DNB/main.py
+++ DNB/main.py
@@ -6,74 +6,78 @@ from scipy.stats import ttest_ind_from_stats
 from sklearn.feature_selection import SelectFdr
 from sklearn.cluster import Birch
 from scipy.stats import pearsonr
-#import matplotlib.pyplot as plt
+import configparser
+import warnings
 
+
+
+#参数配置文件    
+def get_options():
+    cf = configparser.ConfigParser()
+    cf.read('config.cof')
+    
+    option_dict = dict()
+    for key,value in cf.items("Main"):
+        option_dict[key] = eval(value)
+
+    return option_dict
+
+#方差过滤
+def var_filter(dataset,percent = 0.8):
+    new_index_series = dataset.var(1)
+    print("the row dataset shape is: " + str(dataset.shape))
+    dataset = dataset.loc[new_index_series != 0,:]
+
+    new_index = new_index_series[new_index_series != 0].sort_values().index
+    new_dataset = dataset.reindex(index = new_index)
+    
+    if percent > 0 and percent < 1:
+        new_dataset = new_dataset.iloc[:int(new_dataset.shape[0]*percent),:]
+    else:
+        print("the percent is error!")    
+
+    print("the var filtered dataset shape: " + str(new_dataset.shape))
+    
+    return new_dataset,new_dataset.index
 
 #加载数据集
-def load_dataset(filename):
-    dataset = pd.read_table(filename,index_col = "GeneID")
-    GeneID = save_GeneID(dataset,filename)
-    dataset_list = [dataset.iloc[:,index:index+5] for index in [0,5,10,15,20]]
+def load_dataset(case_filename,control_filename,option_dict):
 
-    return dataset_list
+    case_dataset = pd.read_table(case_filename,index_col = option_dict["index_column_name"])
+    case_dataset, case_dataset_index = var_filter(case_dataset,percent = option_dict["var_percent"])
 
+    control_dataset = pd.read_table(control_filename,index_col = option_dict["index_column_name"])
+    control_dataset = control_dataset.loc[case_dataset_index,:]
+    
+    case_dataset_list = [case_dataset.iloc[:,index:index + option_dict["sample_for_every_periods"]] for index in range(0,option_dict["periods"] * 
+            option_dict["sample_for_every_periods"],option_dict["sample_for_every_periods"])]
 
-def save_GeneID(dataset,filename):
-    GeneID_dict = dict()
-    GeneID = dataset.index.tolist()
+    control_dataset_list = [control_dataset.iloc[:,index:index + option_dict["sample_for_every_periods"]] for index in range(0,option_dict["periods"] * 
+            option_dict["sample_for_every_periods"],option_dict["sample_for_every_periods"])]
 
-    for index, ID in zip(range(dataset.shape[0]),GeneID):
-        GeneID_dict[index] = ID
-
-    with open("{}_GeneID.pkl".format(filename.split("_")[1]),"wb") as f:
-        pickle.dump(GeneID_dict,f)        
-
-    #print(GeneID_dict)    
-
-"""
-#利用 t_test 得到 p_value
-def t_test(case,control):
-    statistic,p_value = ttest_ind(case,control,axis=1,equal_var=False)
-    #p_value = pd.Series(data=p_value,index=list(range(len(p_value))))
-    print(p_value)
-    return p_value
-"""    
-
-"""
-#use formaular 
-def formaular(case,control):
-    abar = np.mean(case,1)
-    avar = np.var(case,1)
-    na = case.shape[1]
-    adof = na - 1
-
-    bbar = np.mean(control,1)
-    bvar = np.var(control,1)
-    nb = control.shape[1]
-    bdof = nb - 1
-
-    tf = (abar - bbar) / np.sqrt(avar/na + bvar/nb)
-    dof = (avar/na + bvar/nb)**2 / (avar**2/(na**2*adof) + bvar**2/(nb**2*bdof))
-    pf = 2*stdtr(dof, -np.abs(tf))    
-
-    print(pf)
-"""
+    return case_dataset_list,control_dataset_list
+ 
 
 #T-test 
 def t_test(case,control):
     p_mean,n_mean = np.mean(case,1),np.mean(control,1)
+    #print(p_mean)
     p_std,n_std = np.std(case,1),np.std(control,1)
+
     t_value,p_value = ttest_ind_from_stats(p_mean,p_std,case.shape[1],n_mean,n_std,control.shape[1],equal_var=False)
+    p_value[np.isnan(p_value)] = 100
+    
     return p_value
 
 
 #根据 t 检验 然后和 fdr 的结果的过滤数据集
 def fdr_t_value_filter(case,control,fdr=None):
-
     p_value = t_test(case,control)
+    #print(p_value)
     if not fdr:
-        fdr = 0.01
-    index_mask = p_value < 0.01
+        fdr = 1
+
+    index_mask = p_value * fdr < 0.05
 
     filtered_dataset = case.loc[index_mask,:]
     print("t-test filtered dataset shape: " + str(filtered_dataset.shape))
@@ -81,35 +85,33 @@ def fdr_t_value_filter(case,control,fdr=None):
 
 
 
-
 #准备不同时间片的数据集
-def prepare(case_filename, control_filename):
-    case_dataset_list = load_dataset(case_filename)
-    control_dataset_list = load_dataset(control_filename)
-    print("raw dataset shape: " + str(case_dataset_list[0].shape) + "\n")
-    #fdr = max(p_value)/control.shape[0]
+def prepare(option_dict):
+    case_filename, control_filename = option_dict["case_filename"],option_dict["control_filename"]
+    case_dataset_list, control_dataset_list = load_dataset(case_filename,control_filename,option_dict)
+
     dataset_list = []
-    for i in range(5):
+    for i in range(option_dict["periods"]):
+        print("\nusing the t-test and fdr to filtered the dataset......\n")
         print("getting the {} period dataset....".format(i))
         dataset_list.append(fdr_t_value_filter(case_dataset_list[i],control_dataset_list[i],fdr = None))
     print("\n")
+
     return dataset_list,control_dataset_list    
 
 
 
-
-
 #cluster the dataset, return labels and all candidate groups
-def cluster_dataset(case,n_clusters = 50):
-    cluster = Birch(n_clusters = n_clusters)
+def cluster_dataset(case,option_dict):
+    cluster = Birch(n_clusters = option_dict["n_clusters"])
     cluster.fit(case)
     labels = cluster.labels_  #labels   
 
     all_condidate_clusters = []
-    for i in range(50):
+    for i in range(option_dict["n_clusters"]):
         case_cluster = case.iloc[labels == i,:]
         #自己改进的地方   如果聚类的结果中基因的个数小于等于三个，则放弃这个类
-        if case_cluster.shape[0] <=10:
+        if case_cluster.shape[0] <= option_dict["droped_cluster_gene_count"]:
             continue 
         all_condidate_clusters.append(case_cluster)
 
@@ -194,15 +196,15 @@ def get_cluster_pair(all_case_clusters,control_dataset):
 #封装一个work函数， 包含对每一个 cluster 的所有运算， 同时提供给进程池
 def work(cluster_pair):
     case_cluster,control_cluster = cluster_pair
-    #print(case_cluster.shape)
+    print(case_cluster.shape)
     #print(case_cluster)
     #print(control_cluster)
     case_ci = calculate_ci(case_cluster,control_cluster)
     control_ci = calculate_ci(control_cluster,control_cluster)
 
-    #print("the case ci is: " + str(case_ci))
-    #print("the control ci is " + str(control_ci))
-    #print("")
+    print("the case ci is: " + str(case_ci))
+    print("the control ci is " + str(control_ci))
+    print("")
     return case_ci,control_ci
 
 
@@ -223,22 +225,26 @@ def multiprocessing_clusetr(cluster_pair_list):
 
 
     
-def main(case_filename,control_filename):
-    case_dataset_list,control_dataset_list = prepare(case_filename,control_filename)
+def main():
     
+    option_dict = get_options()
+    case_dataset_list,control_dataset_list = prepare(option_dict)
+    
+
     for i in range(len(case_dataset_list)):
-        all_case_clusters, case_labels = cluster_dataset(case_dataset_list[i])
+        
+        all_case_clusters, case_labels = cluster_dataset(case_dataset_list[i],option_dict)
         cluster_pair_list = get_cluster_pair(all_case_clusters,control_dataset_list[i])
-        
-        case_ci_list,control_ci_list = multiprocessing_clusetr(cluster_pair_list[10:15])
-        print("")
-        print("the {} of time period: ".format(i))
-        print("case result: " + str(case_ci_list))
-        print("control result: " + str(control_ci_list))
+
+        case_ci_list,control_ci_list = multiprocessing_clusetr(cluster_pair_list)
+        print("\n")
+        print("the {} of time period: \n".format(i))
+        print("case result: " + str(case_ci_list) + "\n")
+        print("control result: " + str(control_ci_list) + "\n")
                 
-        
+     
 if __name__ == '__main__':
-    main("liver_case_data.txt","liver_control_data.txt")
+    main()
     
 
 
