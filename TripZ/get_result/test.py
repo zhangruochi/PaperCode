04308b7b2c5cfdd4b0dfc2500ec9b8f0a66e6673
zhangruochi <zrc720@gmail.com>
2017-03-18 18:33:56 +0800

    2017.03.17

---
+++ TripZ/get_result/test.py
@@ -0,0 +1,53 @@
+import re
+import numpy as np
+
+content = ""
+with open("acc.txt","r") as f:
+    content = f.read()
+    print(content)
+
+pattern = re.compile("Four acc:  0\.\d+") 
+result = re.findall(pattern,content)
+#print(result)   
+
+result_number = [float(item.strip("Four acc:  ")) for item in result]
+print(result_number)
+
+
+rf = []
+dt = []
+nb = []
+lr = []
+knn = []
+
+for index, number in enumerate(result_number) :
+    if index % 5 == 0:
+        rf.append(number)
+    elif index % 5 == 1:
+        dt.append(number)
+    elif index % 5 == 2:
+        nb.append(number)
+    elif index % 5 == 3:
+        lr.append(number)
+    elif index % 5 == 4:
+        knn.append(number)
+
+
+
+print(len(rf))
+print(len(dt))
+print(len(nb))
+print(len(lr))
+print(len(knn))            
+                
+
+result_list = [rf,dt,nb,lr,knn]
+
+for estimator_result in result_list:
+    print(np.mean(estimator_result))
+    print(np.var(estimator_result))
+    print("")
+
+
+
+
