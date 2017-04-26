c84f502ba5acdda781a68ee44c05763d46f5779e
zhangruochi <zrc720@gmail.com>
2017-04-26 14:58:10 +0800

    2017.04.26

--- brca/parser_class.py
+++ brca/parser_class.py
@@ -1,12 +1,15 @@
 import json
 import pandas as pd
+from collections import defaultdict
 
 #TCGA-A8-A07E 
 
-def get_name_sample_dict():
-    with open("clinical.project-TCGA-BRCA.2017-04-20T02_01_20.302397.json","r") as f:
+
+#解析 json 文件， 得到样本和样本stage的对应dict
+def get_name_sample_dict(json_filename):
+    with open(json_filename,"r") as f:
         dataset = json.load(f)
-        print(type(dataset))
+        #print(type(dataset))
 
         names_classes = dict()
 
@@ -31,34 +34,50 @@ def get_name_sample_dict():
     return names_classes
 
 
-def load_sample():
-    with open("matrix_data.tsv") as f:
+#测试是不是存在名称相同的样本
+def test_exist(samples):
+    exist_sample = defaultdict(int)
+    for sample in samples:
+        exist_sample[sample] += 1
+
+    for sample, num in exist_sample.items():
+        if num != 1:
+            print(sample)  
+
+
+#加载需要训练数据集的类标
+def load_sample(dataset_filename):
+    with open(dataset_filename) as f:
         samples = f.readline().split()     
-        #print(samples)  
+        #test_exist(samples)
+
         samples = [sample[0:12] for sample in samples]
-        #print(samples)
+
         return samples
 
 
 
-
-def get_labels():
+#得到有用的样本及其类标
+def get_labels(dataset_filename,json_filename):
     labels = []
-    name_sample_dict = get_name_sample_dict()
-    samples = load_sample()
-    print("the num of samples: " + str(len(samples)))
-    valid_samples = []
+    name_sample_dict = get_name_sample_dict(json_filename)
+    samples = load_sample(dataset_filename)
+    sample_mask = []   #正确样本的 mask
 
+    index = 0
     for sample_name in samples:
         if sample_name in name_sample_dict:
-            valid_samples.append(sample_name)
+            sample_mask.append(index)
             labels.append(name_sample_dict[sample_name])
-        else:
-            print(sample_name)    
-        
-    print("the valid num of samples: " + str(len(labels)))
+        #不存在 stage 的样本
+        #else:
+        #    print(sample_name)  
+        index += 1
 
-    return valid_samples,labels
+    #print("the num of samples: " + str(len(sample_mask)))    
+    #print("the valid num of samples: " + str(len(labels)))
+
+    return sample_mask,labels
 
 
 
@@ -67,7 +86,8 @@ def get_labels():
 
 
 if __name__ == '__main__':
-    get_labels()
+    get_labels("matrix_data.tsv","clinical.project-TCGA-BRCA.2017-04-20T02_01_20.302397.json")
+
                     
 
 
---
+++ brca/tets.py
@@ -0,0 +1,1 @@
+print("gazhang".strip("ga"))
